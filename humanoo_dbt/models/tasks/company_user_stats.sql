420956afa8acd38d9a3bccd218d39f3866504d23
ces0008 <wvsonp@gmail.com>
2023-12-09 15:46:40 +0100

    Corrected mistakes in caculating %

--- humanoo_dbt/models/tasks/company_user_stats.sql
+++ humanoo_dbt/models/tasks/company_user_stats.sql
@@ -1,21 +1,28 @@
--- analysis/company_user_stats.sql
+-- models/tasks/company_user_stats.sql
 
-WITH users_with_steps  AS (
-  SELECT DISTINCT user_id
+WITH user_count AS (
+  SELECT company_name, COUNT(DISTINCT user_id) AS total_users
   FROM
     {{ ref('users_data') }}
+  GROUP BY 
+    company_name
+),
+
+  users_with_steps  AS (
+  SELECT u.company_name, COUNT(DISTINCT s.user_id) AS users_with_steps
+  FROM
+    {{ ref('users_data') }} u LEFT JOIN main.steps_data s ON u.user_id = s.user_id
+  GROUP BY u.company_name
 ),
 
 final as (
-
 SELECT
-  u.company_name,
-  COUNT(DISTINCT u.user_id) AS users_with_steps,
-  COUNT(DISTINCT us.user_id) AS total_users,
-  100.0 * COUNT(DISTINCT u.user_id) / COUNT(DISTINCT us.user_id) AS percentage_users_with_steps
-FROM {{ ref('users_data') }} u
-LEFT JOIN users_with_steps us ON u.user_id = us.user_id
-GROUP BY u.company_name
+  uc.company_name,
+  uc.total_users,
+  (uws.users_with_steps * 100) / uc.total_users AS percentage_users_with_steps
+FROM
+  user_count uc
+JOIN users_with_steps uws ON uc.company_name = uws.company_name
 )
 
-select * from final
+SELECT * from final
